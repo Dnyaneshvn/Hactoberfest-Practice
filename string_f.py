30eea0ad03721a0ee8e76176cb94fd50b2eda3bb
Dnyanesh Nimbalkar <78072155+Dnyaneshvn@users.noreply.github.com>
2022-10-15 11:49:00 +0530

    added string code

---
+++ string_f.py
@@ -0,0 +1,9 @@
+name = "Dnyanesh Nimbalkar"
+className = "TE"
+div = "B"
+#Hello I'm Dnyanesh Nimbalkar from TE
+template = f"Hello I\"m {name} from {className}({div}).\n\f\f\fadd ={10+20}"
+# a = f'Hello I\'m {name} from {className}({div}).'
+
+print(template)
+# print(a)
